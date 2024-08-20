c61bbe657c1e9864a5fca1722c7543e5e0f4ad0d
eliphatfs <shiruoxi61@gmail.com>
2024-08-20 13:33:23 +0800

    Delta.

--- previewer/scheduler.py
+++ previewer/scheduler.py
@@ -8,8 +8,8 @@ work = []
 
 
 if __name__ == '__main__':
-    assert 0 == os.system(f"rclone --config rclone.conf copy haosus3:objaverse-xl/meta/u5s_glb.json .nk8s/")
-    with open(".nk8s/u5s_glb.json", "r") as fi:
+    # assert 0 == os.system(f"rclone --config rclone.conf copy haosus3:objaverse-xl/meta/u5s_glb.json .nk8s/")
+    with open(".nk8s/u5s_delta.json", "r") as fi:
         work: list = json.load(fi)
 
     random.shuffle(work)
